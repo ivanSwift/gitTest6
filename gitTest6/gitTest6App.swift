7d90c183be1fe62b002a85103960f94fa62d6783
ivanSwift <terekhovvan@yahoo.com>
2024-09-12 15:45:34 +0300

    Initial Commit

---
+++ gitTest6/gitTest6App.swift
@@ -0,0 +1,17 @@
+//
+//  gitTest6App.swift
+//  gitTest6
+//
+//  Created by Иван Терехов on 12.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct gitTest6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
